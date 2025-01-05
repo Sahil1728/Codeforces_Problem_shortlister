b5577e7b1080468dcbf170d1a03b6c9434ecf77f
Sahil Sharma <shifusahil2811@gmail.com>
2025-01-05 16:34:23 +0000

    Updated the inclusion and exclusion of the problems

--- app/services/problem_fetcher.py
+++ app/services/problem_fetcher.py
@@ -1,7 +1,62 @@
 import requests
+pos = 0
+def in_excluded_tags(prob, excluded):
+    tags = [tag.lower() for tag in prob['tags']]
+    for ex in excluded:
+        if any(ex.lower() in tag for tag in tags):
+            return True
+    return False
 
-def filter_problems(data, usernames, lower_lim, upper_lim):
-    return data
+def in_included_tags(prob, included):
+    tags = [tag.lower() for tag in prob['tags']]
+    for inc in included:
+        if any(inc.lower() in tag for tag in tags):
+            return True
+    return False
+
+def construct_info(stats, prob):
+    global pos
+    pos += 1
+    contestID = prob['contestId']
+    number = prob['index']
+    rating = int(prob.get('rating', '0'))
+    name = prob['name']
+    tags = list(set(prob['tags']))  # some problems have same tags mentioned multiple times
+    solved = stats['solvedCount']
+    return_dict = {
+        'contestID': contestID,     # This is the contest ID of the contest in which the problem appeared
+        'number': number,           # The problem no. eg A, B, C ...
+        'rating': rating,           # Rating of the problem
+        'name': name,               # Name of the problem
+        'tags': tags,               # tags of the problem
+        'solved': solved            # No of people who have solved the problem
+    }
+    return pos, return_dict
+
+def out_of_limit(prob, lower_lim, upper_lim):
+    points = int(prob.get('rating', False))
+    if not points:
+        return False
+    lower_lim = int(lower_lim)
+    upper_lim = int(upper_lim)
+    if points < lower_lim or points > upper_lim:
+        return True
+    return False
+
+def filter_problems(data, usernames, lower_lim, upper_lim, included, excluded):
+    selected_problems = {}
+    if not data['status'] == 'OK':
+        return data
+    total = len(data['result']['problems'])
+    for idx in range(total):
+        stats = data['result']['problemStatistics'][idx]
+        prob = data['result']['problems'][idx]
+        if out_of_limit(prob, lower_lim, upper_lim) or in_excluded_tags(prob, excluded):
+            continue
+        elif in_included_tags(prob, included):
+            key, value = construct_info(stats, prob)
+            selected_problems[key] = value
+    return selected_problems
 
 
 def fetch_problems(usernames, lower_lim, upper_lim, included_list, excluded_list):
@@ -16,5 +71,5 @@ def fetch_problems(usernames, lower_lim, upper_lim, included_list, excluded_list
     if resp.status_code not in [200, 201]:
         return (False, data.get('comment', 'Error in fetching data'))
     else:
-        data = filter_problems(data, usernames, lower_lim, upper_lim)
+        data = filter_problems(data, usernames, lower_lim, upper_lim, included_list, excluded_list)
     return (True, data)
